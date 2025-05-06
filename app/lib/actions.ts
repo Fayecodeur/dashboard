14cacebd7bcbcbddc729cfc6bf8b494539cd45d5
Fayecodeur <fayecodeur99@gmail.com>
2025-05-06 15:29:25 +0000

    Correction des bugs

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,132 +1,142 @@
-"use server"
-import { signIn } from '@/auth';
-import { sql } from '@vercel/postgres';
-import { AuthError } from 'next-auth';
-import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
-import { z } from 'zod';
+"use server";
+import { signIn } from "@/auth";
+import { sql } from "@vercel/postgres";
+import { AuthError } from "next-auth";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
 
 export type State = {
-    errors?: {
-        customerId?: string[];
-        amount?: string[];
-        status?: string[];
-    };
-    message?: string | null;
-}
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string({
-        invalid_type_error: 'Veuillez sélectionner un client',
-    }),
-    amount: z.coerce
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: "Veuillez sélectionner un client",
+  }),
+  amount: z.coerce
     .number()
-    .gt(0, { message: 'Veuillez entrer un montant supérieur à $0.'}),
-    date: z.string(),
-    status: z.enum(['pending', 'paid'], {
-        invalid_type_error: 'Veuillez sélectionner le status de la facture.'
-    })
-})
+    .gt(0, { message: "Veuillez entrer un montant supérieur à $0." }),
+  date: z.string(),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Veuillez sélectionner le status de la facture.",
+  }),
+});
 
-const CreateInvoice = FormSchema.omit({id: true, date: true});
-const UpdateInvoice = FormSchema.omit({id: true, date: true});
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
 
-    const validatedFields = CreateInvoice.safeParse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    })
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Champs manquants. Échec de la création de la facture.",
+    };
+  }
 
-    if (!validatedFields.success) {
-        return {
-            errors: validatedFields.error.flatten().fieldErrors,
-            message: 'Champs manquants. Échec de la création de la facture.'
-        }
-    }
-    
-    const {customerId, amount, status} = validatedFields.data;
+  const { customerId, amount, status } = validatedFields.data;
 
-    const amountInCents = amount * 100;
+  const amountInCents = amount * 100;
 
-    const date = new Date().toISOString().split('T')[0];
+  const date = new Date().toISOString().split("T")[0];
 
-    try {
-         // Enregistrer la facture en BDD
-        await sql`
+  try {
+    // Enregistrer la facture en BDD
+    await sql`
             INSERT INTO invoices (customer_id, amount, status, date)
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
-    } catch (error) {
-        return { message: 'Erreur base de données: échec lors de la création de la facture.'}
-    }
+  } catch (error) {
+    return {
+      message:
+        "Erreur base de données: échec lors de la création de la facture.",
+    };
+  }
 
-    // Revalider le path (vider le cache de route côté client et le mettre à jour)
-    revalidatePath('/dashboard/invoices');
+  // Revalider le path (vider le cache de route côté client et le mettre à jour)
+  revalidatePath("/dashboard/invoices");
 
-    // Redirection vers dashboard/invoices après enregistrement en BDD.
-    redirect('/dashboard/invoices');
+  // Redirection vers dashboard/invoices après enregistrement en BDD.
+  redirect("/dashboard/invoices");
 }
 
-export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
+  const validatedFields = UpdateInvoice.safeParse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
 
-    const validatedFields = UpdateInvoice.safeParse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    })
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Champs manquants. Échec de la création de la facture.",
+    };
+  }
 
-    if (!validatedFields.success) {
-        return {
-            errors: validatedFields.error.flatten().fieldErrors,
-            message: 'Champs manquants. Échec de la création de la facture.'
-        }
-    }
-    
-    const {customerId, amount, status} = validatedFields.data;
-    
-    const amountInCents = amount * 100;
+  const { customerId, amount, status } = validatedFields.data;
 
-    try {
-         // Modifier la facture en BDD avec un ID spécifique
-        await sql`
+  const amountInCents = amount * 100;
+
+  try {
+    // Modifier la facture en BDD avec un ID spécifique
+    await sql`
             UPDATE invoices
             SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
             WHERE id = ${id}
         `;
-    } catch (error) {
-        return { message: 'Erreur base de données: échec lors de la mise à jour de la facture.'}
-    }
+  } catch (error) {
+    return {
+      message:
+        "Erreur base de données: échec lors de la mise à jour de la facture.",
+    };
+  }
 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-    // throw new Error("Échec de la suppression de la facture");
-    
-    try {
-        await sql`DELETE FROM invoices WHERE id = ${id}`;
-        revalidatePath('/dashboard/invoices');
-    } catch (error) {
-        return { message: 'Erreur base de données: échec lors de la suppression de la facture.'}
-    }
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+  } catch (error) {
+    console.error("Erreur suppression facture :", error);
+    // On ne retourne rien ici, sinon cela casse <form action={...}>
+  }
 }
 
-export async function authenticate(prevState: string | undefined, formData: FormData) {
-    try {
-        await signIn('credentials', formData);
-    } catch (error) {
-        if (error instanceof AuthError) {
-            switch (error.type) {
-                case 'CredentialsSignin':
-                    return 'Identifiants invalides.';
-                default:
-                    return "Quelque chose s'est mal passé.";
-            }
-        }
-        throw error;
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Identifiants invalides.";
+        default:
+          return "Quelque chose s'est mal passé.";
+      }
     }
+    throw error;
+  }
 }
